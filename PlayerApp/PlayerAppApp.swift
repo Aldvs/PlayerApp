633dddd21339dc3c73925de25c09bc7cb02d7234
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-09 20:52:36 +0300

    Initial Commit

---
+++ PlayerApp/PlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerAppApp.swift
+//  PlayerApp
+//
+//  Created by admin on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
