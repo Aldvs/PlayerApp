9308401932384ae9e18b5083a36e8da36f5e80c7
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-10 08:30:46 +0300

    Setup button

--- PlayerApp/ContentView.swift
+++ PlayerApp/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var performAnimation = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {
+            withAnimation(Animation.spring(duration: 0.5, bounce: 0.5)) {
+                performAnimation = true
+            }
+        } label: {
+            GeometryReader { proxy in
+                let width = proxy.size.width / 2
+                let imageName = "play.fill"
+                HStack(alignment: .center, spacing: 0) {
+                    Image(systemName: imageName)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: .zero)
+                    Image(systemName: imageName)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: width)
+                    Image(systemName: imageName)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: width)
+                }
+                .frame(maxHeight: .infinity, alignment: .center)
+                .border(.black, width: 2)
+            }
         }
-        .padding()
+        .frame(maxWidth: 62)
+        .border(.purple, width: 1)
     }
 }
 
